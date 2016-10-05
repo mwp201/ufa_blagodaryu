bbe72a8d61bd606c978f15707c8f8363a6a6c6ac
Vjacheslav Kuznetsov <mwp201@yandex.ru>
2016-10-05 16:20:47 +0800

    mail work 05_10_2016

--- send_mail.php
+++ send_mail.php
@@ -1,55 +1,59 @@
 <?php 
 //Если форма отправлена
   if (isset($_POST['submit'])) {
-           //Проверка Поля ИМЯ
-          if (trim($_POST['user_name']) == '') {
-            $hasError = true;
-            } else {
-                $name =substr(htmlspecialchars(trim($_POST['user_name'])), 0, 50); 
-                }
- 
- //Проверка правильности ввода EMAIL
-  if (trim($_POST['user_email']) == '')  {
-  $hasError = true;
-  } else if (!preg_match("/^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,4})$/", trim($_POST['user_email']))) {
-  $hasError = true;
-  } else {
-  $email =substr(htmlspecialchars(trim($_POST['user_email'])), 0, 30);  
-  }
- //Проверка наличия ТЕКСТА сообщения
-  if(trim($_POST['letter_text']) == '') {
-  $hasError = true;
-  } else {
-  if(function_exists('stripslashes')) {
-  $comments = stripslashes(trim($_POST['letter_text']));
-  } else {
-  $comments = wordwrap(htmlspecialchars(trim($_POST['letter_text'])), 50, "\r\n"); 
-  }
+      //проверка имени
+      if (trim($_POST['user_name']) == '') {$error = true;}
+      else {$name = substr(htmlspecialchars(trim($_POST['user_name'])), 0, 50);}
+       //Проверка правильности ввода EMAIL
+      if (trim($_POST['user_email']) == '') {$error = true;}
+      else if (!preg_match("/^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,4})$/", trim($_POST['user_email']))) { $error = true;}
+      else {$email = substr(htmlspecialchars(trim($_POST['user_email'])), 0, 30);}
+     //Проверка наличия ТЕКСТА сообщения
+      if(trim($_POST['letter_text']) == '') {$error = true;}
+      else if(function_exists('stripslashes')) {$text = wordwrap(stripslashes(trim($_POST['letter_text'])), 50, "\r\n");}
+      else {$text = wordwrap(htmlspecialchars(trim($_POST['letter_text'])), 50, "\r\n"); }
   }
  //Если ошибок нет, отправить email
-  if(!isset($hasError)) {
+  if(!isset($error)) {
     $emailTo = 'mwp201@yandex.ru'; //Сюда введите Ваш email
-    $body = 'Имя отправителя :'.$name."\r\n".'Email отправителя :'.$email."\r\n".'Тест сообщения :'.$comments;
+    $body = '<p>Имя отправителя : '.$name.'</p><br>'.'<p>Email отправителя : '.$email.'</p><br>'.'<p>Тест сообщения : '.$text.'</p>';
     mail($emailTo, $body, $headers);
     $emailSent = true;
   }
-      if ($emailSent == true) echo 'Спасибо! Ваше письмо отправлено.'; 
-      else if ($hasError == true) echo 'Ваше письмо не отправлено! <br> Проверьте правильность заполнения полей!'; 
-  }
-?>
+  ?>
+
+
 
 
     <html>
 
     <head>
         <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/style.css">
         <title>Отчет об отправлении</title>
     </head>
 
     <body>
-        <a href="index.html">
-            <input type="button" value="НАЗАД">
-        </a>
+        <div class="fixed-overlay" id="send">
+            <div class="modal">
+                <div class="modal-window-send clearfix">
+                    <?php 
+      if ($emailSent == true) {echo '<p style="color:#000;">Спасибо! Ваше письмо отправлено.</p>';} 
+      else if ($error == true) {echo '<p>Ваше письмо не отправлено! <br> Проверьте правильность заполнения полей!</p>';} 
+    ?>
+                        <a class="btn" href="index.html">Вернуться назад</a>
+                </div>
+            </div>
+        </div>
+        <script>
+            window.setTimeout(function () {
+                location.href = 'index.html';
+            }, 5000);
+        </script>
     </body>
 
     </html>

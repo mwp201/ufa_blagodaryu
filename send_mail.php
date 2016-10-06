cb2f4d2f62e336eba6f7af5a0737a38562b88b8f
Vjacheslav Kuznetsov <mwp201@yandex.ru>
2016-10-06 14:00:34 +0800

    test version OK! 06_10_2016

--- send_mail.php
+++ send_mail.php
@@ -3,27 +3,48 @@
   if (isset($_POST['submit'])) {
       //проверка имени
       if (trim($_POST['user_name']) == '') {$error = true;}
-      else {$name = substr(htmlspecialchars(trim($_POST['user_name'])), 0, 50);}
+      else {$name = substr(htmlspecialchars(trim($_POST['user_name'])), 0, 80);}
        //Проверка правильности ввода EMAIL
       if (trim($_POST['user_email']) == '') {$error = true;}
       else if (!preg_match("/^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,4})$/", trim($_POST['user_email']))) { $error = true;}
-      else {$email = substr(htmlspecialchars(trim($_POST['user_email'])), 0, 30);}
+      else {$email = substr(htmlspecialchars(trim($_POST['user_email'])), 0, 50);}
      //Проверка наличия ТЕКСТА сообщения
       if(trim($_POST['letter_text']) == '') {$error = true;}
       else if(function_exists('stripslashes')) {$text = wordwrap(stripslashes(trim($_POST['letter_text'])), 50, "\r\n");}
       else {$text = wordwrap(htmlspecialchars(trim($_POST['letter_text'])), 50, "\r\n"); }
   }
- //Если ошибок нет, отправить email
-  if(!isset($error)) {
-    $emailTo = 'mwp201@yandex.ru'; //Сюда введите Ваш email
-    $body = '<p>Имя отправителя : '.$name.'</p><br>'.'<p>Email отправителя : '.$email.'</p><br>'.'<p>Тест сообщения : '.$text.'</p>';
-    mail($emailTo, $body, $headers);
-    $emailSent = true;
-  }
+$subject = 'Письмо с сайта магазина БлагоДарю';
+$header  = "MIME-Version: 1.0 \r\n";
+$header .= "Content-type: text/html; charset=utf-8 \r\n"; 
+$header .= "From: ".$email."\r\n";
+$emailTo = 'mwp201@yandex.ru'; //Сюда введите Ваш email
+$message = '
+<!DOCTYPE html>
+<html>
+<head>
+<title>Birthday</title>
+</head>
+<body>
+<table width="100%">
+<tr>
+<td>
+<h4>Имя отправителя : '.$name.'</h4>
+<h4>Email отправителя : '.$email.'</h4>
+<p>Текст сообщения : '.$text.'</p>
+</td>
+</tr>
+</table>
+</body>
+</html>';
+//Если ошибок нет, отправить email
+if(!isset($error)) {
+ mail($emailTo, $subject, $message, $header);
+$emailSent = true;
+}
   ?>
 
 
-
+    <!DOCTYPE html>
 
     <html>
 
